8df584bfdff49e96cc372ed10aba72d8fec80281
ktopchiev <karlostopchiev@gmail.com>
2021-04-23 15:18:51 +0300

    .

---
+++ JS-Advanced/01. Syntax, Functions and Statements/07.Day Of Week.js
@@ -0,0 +1,35 @@
+function dayOfWeekNumber(input) {
+    
+    let number = 0;
+
+    switch(input) {
+        case 'Monday':
+            number = 1;
+            break;
+        case 'Tuesday':
+            number = 2;
+            break;
+        case 'Wednesday':
+            number = 3;
+            break;
+        case 'Thursday':
+            number = 4;
+            break;
+        case 'Friday':
+            number = 5;
+            break;
+        case 'Saturday':
+            number = 6;
+            break;
+        case 'Sunday':
+            number = 7;
+            break;
+        default:
+            number = 'error';
+            break;
+    }
+
+    console.log(number)
+}
+
+dayOfWeekNumber('Wednesday');
