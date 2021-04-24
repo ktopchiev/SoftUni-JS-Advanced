8a7e7b5f4e6927f57b69c604f34e633f02f7636b
ktopchiev <karlostopchiev@gmail.com>
2021-04-24 12:31:55 +0300

    gcd solved

---
+++ JS-Advanced/01. Syntax, Functions and Statements Exercises/02.Greatest Common Divisor - GCD.js
@@ -0,0 +1,13 @@
+function findGreatestCommonDivisor(x, y) {
+    
+    x = Math.abs(x);
+    y = Math.abs(y);
+
+    while(y) {
+        var t = y;
+        y = x % y;
+        x = t;
+    }
+    
+    return x;
+}
