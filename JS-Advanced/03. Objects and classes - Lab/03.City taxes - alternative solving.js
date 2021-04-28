cb81d74f5b300cc479ecf1c92add825faeaa19f0
ktopchiev <karlostopchiev@gmail.com>
2021-04-28 16:17:21 +0300

    .

---
+++ JS-Advanced/03. Objects and classes - Lab/03.City taxes - alternative solving.js
@@ -0,0 +1,23 @@
+
+function cityTaxes(name, population, treasury) {
+    const city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor((this.population * percentage) / 100);
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.ceil((this.treasury * percentage) / 100);
+        },
+    };
+
+    return city;
+}
+
+const city = cityTaxes('Dimitrovgrad', 30000, 1000000);
+console.log(city.name);
