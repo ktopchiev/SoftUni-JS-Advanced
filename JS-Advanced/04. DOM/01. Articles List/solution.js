e963c8ea763338b22f0bacae6359b91ee073c51e
ktopchiev <karlostopchiev@gmail.com>
2021-05-12 13:27:51 +0300

    Dom

---
+++ JS-Advanced/04. DOM/01. Articles List/solution.js
@@ -0,0 +1,18 @@
+function createArticle() {
+	
+	let titleInput = document.getElementById('createTitle');
+	let contentInput = document.getElementById('createContent');
+	let listOfArticless = document.createElement('ul');
+	let article = document.createElement('li');
+	let articleTitle = document.createElement('h3');
+	let articleContent = document.createElement('p');
+	articleTitle.innerHTML = titleInput.value;
+	articleContent.innerHTML = contentInput.value;
+	article.appendChild(articleTitle);
+	article.appendChild(articleContent);
+	let articlesSection = document.getElementById('articles');
+	articlesSection.appendChild(listOfArticless);
+	listOfArticless.appendChild(article);
+	titleInput.value = '';
+	contentInput.value = '';
+}
