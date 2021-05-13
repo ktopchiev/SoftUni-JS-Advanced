a802dac29c493fa7f54b97787631af91ba6ddcdc
ktopchiev <karlostopchiev@gmail.com>
2021-05-14 02:08:35 +0300

    .

--- JS-Advanced/04. DOM/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS-Advanced/04. DOM/07.Extract Parenthesis/ExtractParenthesis.js
@@ -2,6 +2,15 @@ function extract(content) {
     let text = document.getElementById(content).innerHTML;
 
     let regExp = /\(([^)]+)\)/g;
+    let matches = [...text.matchAll(regExp)];
+    let result = [];
 
-    return text.match(regExp).join(';');
+
+    for (let row = 0; row < matches.length; row++) {
+        for (let col = 1; col < matches[row].length; col++) {
+            result.push(matches[row][col])
+        }
+
+    }
+    return result.join(';');
 }
