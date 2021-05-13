a16d82c98d520b28452dab92c33f3a3761c9c5ec
ktopchiev <karlostopchiev@gmail.com>
2021-05-14 00:24:26 +0300

    show more solved

--- JS-Advanced/04. DOM/04.Show More/ShowMore.js
+++ JS-Advanced/04. DOM/04.Show More/ShowMore.js
@@ -1,3 +1,8 @@
 function showText() {
-    // TODO
+    let textElement = document.getElementById('text');
+    let text = textElement.innerHTML;
+    let readMoreLink = document.getElementById('more');
+    readMoreLink.setAttribute('style', 'display:none');
+    textElement.setAttribute('style','display: inline');
+
 }
