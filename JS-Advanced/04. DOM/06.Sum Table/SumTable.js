1fb2ea478f25ee9f94ac56b0e3f3f377bc72da75
ktopchiev <karlostopchiev@gmail.com>
2021-05-14 00:47:12 +0300

    solved sumtable

--- JS-Advanced/04. DOM/06.Sum Table/SumTable.js
+++ JS-Advanced/04. DOM/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
+    let tableElement = document.querySelector('table');
+    let tableRows = tableElement.getElementsByTagName('td');
+    let sum = 0;
 
+    for (let i = 1; i < tableRows.length - 2; i += 2) {
+        const num = parseFloat(tableRows[i].innerHTML);
+        sum += num;
+        
+    }
+
+    let sumElement = document.getElementById('sum');
+    sumElement.innerHTML = sum;
 }
