1321bbed39c65c110a8251635d2cd7eedd11ae33
ktopchiev <karlostopchiev@gmail.com>
2021-05-14 00:14:20 +0300

    collect list items

--- JS-Advanced/04. DOM/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/04. DOM/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    const ulItems = document.getElementById('items');
+    const li = ulItems.getElementsByTagName('li');
+    const textArea = document.getElementById('result');
+
+    for (let i = 0; i < ulItems.childElementCount; i++) {
+        const element = li[i].innerHTML;
+        textArea.value += element + '\n';
+    }
 }
