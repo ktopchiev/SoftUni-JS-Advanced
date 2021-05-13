f85f656e63cd2bc637ef500a8ec5b82b9ffa4dca
ktopchiev <karlostopchiev@gmail.com>
2021-05-14 00:35:58 +0300

    colorize table solved

--- JS-Advanced/04. DOM/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced/04. DOM/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    let tableElement = document.querySelector("table");
+    let tableRowElement = tableElement.getElementsByTagName('tr');
+
+    for (let i = 1; i < tableRowElement.length; i += 2) {
+        const element = tableRowElement[i];
+        element.style.backgroundColor = 'Teal';
+    }
 }
