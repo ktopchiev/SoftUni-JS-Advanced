f2da7f0a74370ca24e1bbe552bdbda777b094d02
ktopchiev <karlostopchiev@gmail.com>
2021-05-24 14:41:26 +0300

    .

---
+++ JS-Advanced/01. Syntax, Functions and Statements Exercises/08.Words Uppercase.js
@@ -0,0 +1,8 @@
+function stringToUpper(string) {
+    const regx = /[\s,!?]/;
+    let wordsArr = string.split(regx).map(x => x.toUpperCase());
+    let result = wordsArr.filter(x => x !== null && x !== '');
+    console.log(result.join(', '));
+}
+
+stringToUpper('Hi,');
