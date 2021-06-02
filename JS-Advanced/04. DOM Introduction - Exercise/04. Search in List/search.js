a57dfe634a9ae0b37b20516f2abf2a7c46aa4aae
ktopchiev <karlostopchiev@gmail.com>
2021-06-02 21:18:41 +0300

    search

--- JS-Advanced/04. DOM Introduction - Exercise/04. Search in List/search.js
+++ JS-Advanced/04. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let list = document.getElementById('towns');
+   let listElements = list.getElementsByTagName('li');
+   let result = document.getElementById('result');
+   let input = document.getElementById('searchText').value;
+   let counter = 0;
+
+   if (input !== '') {
+      for (let i = 0; i < listElements.length; i++) {
+         if (listElements[i].innerText.includes(input)) {
+            listElements[i].style.fontWeight = 'bold';
+            listElements[i].style.textDecoration = 'underline';
+            counter++;
+         }
+      }
+   }
+   
+   result.innerText = `${counter} matches found`;
 }
