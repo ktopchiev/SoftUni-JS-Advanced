49244bf95bf7ea6e689cf4a0244a760344917455
ktopchiev <karlostopchiev@gmail.com>
2021-06-02 20:26:49 +0300

    pascal or camel case

--- JS-Advanced/04. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/04. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+  let result = document.getElementById('result');
+
+  if (namingConvention === 'Pascal Case') {
+    result.innerHTML = toPascalCase(text);
+  } else if (namingConvention === 'Camel Case') {
+    result.innerHTML = toCamelCase(text);
+  } else {
+    result.innerHTML = 'Error!'
+  }
+
+  function toPascalCase(string) {
+    text = string.split(' ')
+      .reduce((str, current) => {
+        str += (current.substr(0, 1).toUpperCase() + current.substr(1).toLowerCase());
+        return str;
+      }, '');
+    return text;
+  }
+
+  function toCamelCase(string) {
+    string = string.split(' ');
+    let firstWord = string[0].toLowerCase();
+    string.shift();
+    text = toPascalCase(string.join(' '));
+    text = firstWord + text;
+    return text;
+  }
 }
