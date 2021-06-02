2791c73a1c51a1e4bcdf5713f11c9a4348d43c4a
ktopchiev <karlostopchiev@gmail.com>
2021-06-02 22:06:16 +0300

    table searcjh solved

--- JS-Advanced/04. DOM Introduction - Exercise/05. Table - Serach Engine/solution.js
+++ JS-Advanced/04. DOM Introduction - Exercise/05. Table - Serach Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let table = document.getElementsByTagName('tbody')[0];
+      let tableData = table.getElementsByTagName('td');
+      let inputSearchString = document.getElementById('searchField').value;
 
+      for (let i = 0; i < tableData.length; i++) {
+         if (tableData[i].innerText.includes(inputSearchString)) {
+            tableData[i].parentElement.classList.add("select");
+         }
+         
+      }
+      
+      let inputField = document.getElementById('searchField');
+      inputField.value = '';
    }
 }
