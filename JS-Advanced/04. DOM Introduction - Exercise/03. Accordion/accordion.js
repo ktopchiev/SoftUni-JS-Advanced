caa1ec09126c776e7ec79a97635e6d3d18a7ddeb
ktopchiev <karlostopchiev@gmail.com>
2021-06-02 20:47:07 +0300

    solved accordion

--- JS-Advanced/04. DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS-Advanced/04. DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.getElementsByClassName('button')[0];
+    let p = document.getElementById('extra');
+
+    if (p.style.display === 'none') {
+        p.style.display = 'block';
+        button.innerHTML = 'Less';
+    } else {
+        p.style.display = 'none';
+        button.innerHTML = 'More';
+    }
 }
