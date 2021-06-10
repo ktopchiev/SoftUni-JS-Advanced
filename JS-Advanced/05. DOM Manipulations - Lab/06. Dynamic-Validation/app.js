3411e7edd8cfbf4ff7ed567a31df6e24b2b43e71
ktopchiev <karlostopchiev@gmail.com>
2021-06-10 17:39:47 +0300

    Dynamic validation solved

--- JS-Advanced/05. DOM Manipulations - Lab/06. Dynamic-Validation/app.js
+++ JS-Advanced/05. DOM Manipulations - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    let rgx = /^[a-z]+@([a-z]+\.)+[a-z]{2,4}\b/;
+
+    inputElement.addEventListener('change', (e) => {
+        if (!e.target.value.match(rgx)) {
+            e.currentTarget.classList.add("error");
+        } else {
+            e.currentTarget.classList.remove("error");
+        }
+    })
 }
