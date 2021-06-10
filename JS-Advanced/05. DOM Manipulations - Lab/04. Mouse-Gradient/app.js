ed5099078fb28012fb029b5dd774776384d494a3
ktopchiev <karlostopchiev@gmail.com>
2021-06-10 15:52:28 +0300

    gradient solved

--- JS-Advanced/05. DOM Manipulations - Lab/04. Mouse-Gradient/app.js
+++ JS-Advanced/05. DOM Manipulations - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,10 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultDivElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', (e) => {
+        let width = gradientElement.offsetWidth - 1;
+        let percent = Math.floor((e.offsetX / width) * 100);
+        resultDivElement.textContent = percent + '%';
+    })
 }
