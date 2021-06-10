ca930d824b0fa26bedcc44de492776adc91e6b9a
ktopchiev <karlostopchiev@gmail.com>
2021-06-10 11:59:15 +0300

    .

--- JS-Advanced/05. DOM Manipulations - Lab/03. Delete-From-Table/app.js
+++ JS-Advanced/05. DOM Manipulations - Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let table = document.getElementById('customers');
+    let input = document.querySelector('input[type="text"').value;
+    let tableBody = table.getElementsByTagName('tbody')[0];
+    let resultDiv = document.getElementById('result');
+    let found = false;
+
+    for (let i = 0; i < tableBody.children.length; i++) {
+        if (tableBody.children[i].querySelector('td:nth-child(2)').innerText === input) {
+           tableBody.removeChild(tableBody.children[i]);
+            resultDiv.innerText = 'Deleted.';
+            found = true;
+        }
+    }
+
+    if (found === false) {
+        resultDiv.innerText = 'Not found.';
+    }
 }
