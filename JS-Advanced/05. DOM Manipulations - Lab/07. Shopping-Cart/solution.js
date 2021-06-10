f0f8cfb62a32451e7c143eebcc1da67113ff728a
ktopchiev <karlostopchiev@gmail.com>
2021-06-10 19:31:35 +0300

    shopping cart solved

--- JS-Advanced/05. DOM Manipulations - Lab/07. Shopping-Cart/solution.js
+++ JS-Advanced/05. DOM Manipulations - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-   //TODO...
+   let textAreaElement = document.querySelector('textArea');
+   let buttons = document.querySelectorAll('button.add-product');
+   let list = [];
+   let totalPrice = 0;
+
+   textAreaElement.removeAttribute('disabled');
+
+   for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', (event) => {
+         let product = event.target.parentNode.parentNode;
+         let title = product.querySelector('.product-details').firstElementChild.innerText;
+         let price = product.querySelector('.product-line-price').innerText;
+
+         list.includes(title) ? null : list.push(title);
+         totalPrice += Number(price);
+         textAreaElement.value += `Added ${title} for ${price} to the cart.\n`;
+      });
+   }
+
+   document.getElementsByClassName('checkout')[0]
+      .addEventListener('click', (e) => {
+         textAreaElement.value
+            += `You bought ${list.join(', ')} for ${totalPrice.toFixed(2)}.\n`;
+         for (let i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+         }
+         e.target.disabled = true;
+      });
+
 }
