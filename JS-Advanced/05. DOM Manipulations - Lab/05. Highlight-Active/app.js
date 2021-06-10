d58353e586d825dec9ab2a0bbc0cb3b8e5c7e67b
ktopchiev <karlostopchiev@gmail.com>
2021-06-10 17:04:10 +0300

    highlight active solved

--- JS-Advanced/05. DOM Manipulations - Lab/05. Highlight-Active/app.js
+++ JS-Advanced/05. DOM Manipulations - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    let sections = document.querySelectorAll('input[type="text"]');
+
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].addEventListener('focus', (e) => {
+            sections[i].parentElement.classList.add('focused');
+        });
+        
+        sections[i].addEventListener('blur', (e) => {
+            sections[i].parentElement.classList.remove('focused');
+        });
+    }
+
 }
