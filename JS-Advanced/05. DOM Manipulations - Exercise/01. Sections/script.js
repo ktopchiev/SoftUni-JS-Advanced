e0478dc2b63340562fe70026f2d85d7579e03107
ktopchiev <karlostopchiev@gmail.com>
2021-06-11 13:32:51 +0300

    sections solved

--- JS-Advanced/05. DOM Manipulations - Exercise/01. Sections/script.js
+++ JS-Advanced/05. DOM Manipulations - Exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   let divContentElement = document.getElementById('content');
+
+   words.forEach(word => {
+      let contentElement = document.createElement('div');
+      let contentParagraph = document.createElement('p');
+      contentParagraph.innerText = word;
+      contentParagraph.style.display = 'none';
+      contentElement.appendChild(contentParagraph);
+      contentElement.addEventListener('click', (e) => {
+         e.currentTarget.firstElementChild.style.display = 'block';
+      })
+      divContentElement.appendChild(contentElement);
+   });
 }
