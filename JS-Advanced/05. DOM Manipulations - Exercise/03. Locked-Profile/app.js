105e84f1c5aa2a7dc099c00fecbb5317790f5f27
ktopchiev <karlostopchiev@gmail.com>
2021-06-12 12:26:57 +0300

    solved

--- JS-Advanced/05. DOM Manipulations - Exercise/03. Locked-Profile/app.js
+++ JS-Advanced/05. DOM Manipulations - Exercise/03. Locked-Profile/app.js
@@ -1,35 +1,30 @@
 function lockedProfile() {
     let profiles = document.querySelectorAll('div.profile');
 
-    //Initial settings
+ 
     for (let i = 0; i < profiles.length; i++) {
-        profiles[i].querySelector(`#user${i + 1}HiddenFields`).style.display = 'none';
-        profiles[i].querySelector('input[value="lock"]').checked = true;
-        profiles[i].querySelector('button').disabled = true;
-    }
-
-    for (let i = 0; i < profiles.length; i++) {
-
+ 
         profiles[i].querySelector('div.profile input[value="lock"]').addEventListener('click', (e) => {
             e.target.parentNode.querySelector('button').disabled = true;
         });
         profiles[i].querySelector('div.profile input[value="unlock"]').addEventListener('click', (e) => {
             e.target.parentNode.querySelector('button').disabled = false;
         });
-
+ 
         profiles[i].querySelector('button')
             .addEventListener('click', (e) => {
                 let buttonShowHide = e.target;
-
+                console.log(e.target);
                 let userInfoDiv = buttonShowHide.parentNode.querySelector('div');
-
-                if (!buttonShowHide.disabled) {
-                    if (userInfoDiv.style.display === 'none') {
-                        userInfoDiv.style.display = 'block';
-                        buttonShowHide.innerText = 'Hide it';
-                    } else {
+                let radioButton = buttonShowHide.parentNode.querySelector('input[type="radio"]:checked');
+ 
+                if (radioButton.value == 'unlock') {
+                    if (userInfoDiv.style.display === 'block') {
                         userInfoDiv.style.display = 'none';
                         buttonShowHide.innerText = 'Show more';
+                    } else {
+                        userInfoDiv.style.display = 'block';
+                        buttonShowHide.innerText = 'Hide it';
                     }
                 }
             });
--- JS-Advanced/05. DOM Manipulations - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/05. DOM Manipulations - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,33 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let messageArea = document.querySelector('textarea[placeholder="Write your message here..."');
+    let receivedMsgArea = document.querySelector('textarea[placeholder="No messages..."');
+    let sendButton = document.querySelectorAll('button')[0];
+    let readButton = document.querySelectorAll('button')[1];
+
+    sendButton.addEventListener('click', (e) => {
+        let message = messageArea.value;
+        let encodedMsg = '';
+        for (let i = 0; i < message.length; i++) {
+            let newCode = message.charCodeAt(i) + 1;
+            encodedMsg += String.fromCharCode(newCode);
+        }
+
+        receivedMsgArea.value = encodedMsg;
+        messageArea.value = '';
+        readButton.disabled = false;
+    });
+
+    readButton.addEventListener('click', (e) => {
+        
+        let receivedMsg = receivedMsgArea.value;
+        let decodedMsg = '';
+        for (let i = 0; i < receivedMsg.length; i++) {
+            let currentCharCode = receivedMsg.charCodeAt(i);
+            currentCharCode -= 1;
+            decodedMsg += String.fromCharCode(currentCharCode);
+        }
+
+        receivedMsgArea.value = decodedMsg;
+        
+    });
 }
