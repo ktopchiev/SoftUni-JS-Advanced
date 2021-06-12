9de43aa235ac0b4c4737837d5df1049edae360db
ktopchiev <karlostopchiev@gmail.com>
2021-06-12 16:28:46 +0300

    changes

--- JS-Advanced/05. DOM Manipulations - Exercise/06. Furniture/solution.js
+++ JS-Advanced/05. DOM Manipulations - Exercise/06. Furniture/solution.js
@@ -14,6 +14,8 @@ function solve() {
     newFurniture.forEach(element => {
       let tableRow = document.createElement('tr');
       let furnitureObject = element;
+
+      //Create table data
       let td;
       for (const [key, value] of Object.entries(furnitureObject)) {
         td = document.createElement('td');
@@ -34,8 +36,9 @@ function solve() {
       checkbox.type = 'checkbox';
       td = document.createElement('td');
       td.appendChild(checkbox);
+
+      //Append table data to new table row
       tableRow.appendChild(td);
-      tableRow.querySelector('input[type="checkbox"]').disabled = false;
       tableBody.appendChild(tableRow);
     });
   });
@@ -44,7 +47,6 @@ function solve() {
     let names = [];
     let price = 0;
     let decFactor = 0;
-    let counter = 0;
 
     for (let i = 0; i < tableBody.children.length; i++) {
       let product = tableBody.children[i];
@@ -53,14 +55,13 @@ function solve() {
         names.push(paragraphs[0].textContent);
         price += Number(paragraphs[1].textContent);
         decFactor += Number(paragraphs[2].textContent);
-        counter += 1;
       }
     }
 
     let boughtFurniture = `Bought furniture: ${names.join(', ')}`;
     price = price.toFixed(2);
-    decFactor /= counter;
+    decFactor /= names.length;
 
-    outputArea.value = `${boughtFurniture}\nTotal price: ${price}\nAverage decoration factor: ${decFactor.toFixed(2)}`;
+    outputArea.value = `${boughtFurniture}\nTotal price: ${price}\nAverage decoration factor: ${decFactor}`;
   });
 }
