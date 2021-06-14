bebb9e0ae8e36d2b8c023f0ff5e2a2c909889185
ktopchiev <karlostopchiev@gmail.com>
2021-06-14 23:44:18 +0300

    .

---
+++ JS-Advanced/06. Advanced functions - Lab/01. Area and volume calculator.js
@@ -0,0 +1,24 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+function solve(area, vol, input) {
+
+    let objects = JSON.parse(input);
+    
+    function calc(obj) {
+        let areaObj = Math.abs(area.call(obj));
+        let volObj = Math.abs(vol.call(obj));
+        return {area: areaObj, volume: volObj};
+    }
+
+    return objects.map(calc);
+}
+
+let input = '[{"x":"1","y":"2","z":"10"},{"x":"7","y":"7","z":"10"},{"x":"5","y":"2","z":"10"}]';
+
+console.log(solve(area, vol, input));
