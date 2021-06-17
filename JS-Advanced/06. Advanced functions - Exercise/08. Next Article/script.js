5d8260349c09b595be5b5de9568f5320b352ab8d
ktopchiev <karlostopchiev@gmail.com>
2021-06-17 23:26:34 +0300

    articles

--- JS-Advanced/06. Advanced functions - Exercise/08. Next Article/script.js
+++ JS-Advanced/06. Advanced functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    return function showNext() {
+        if (articles.length > 0) {
+            let articlesArray = articles;
+            let article = document.createElement('article');
+            article.innerText = articlesArray.shift();
+            if (article.innerText !== 'undefined') {
+                let content = document.getElementById('content');
+                content.appendChild(article);
+            }
+        }
+    }
 }
