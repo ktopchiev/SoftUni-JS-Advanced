4c10960f86f936fd0d4775430c54115448e29111
ktopchiev <karlostopchiev@gmail.com>
2021-07-05 19:02:54 +0300

    solved bus stop problem

--- JS-Applications/02.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ JS-Applications/02.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,31 @@
 function getInfo() {
-    console.log("TODO...");
+    let baseUrl = 'http://localhost:3030/jsonstore/bus/businfo';
+    let stopIdInput = document.getElementById('stopId');
+    let stopNameDivElement = document.getElementById('stopName');
+    let busesList = document.getElementById('buses');
+
+    clearList();
+
+    fetch(`${baseUrl}/${stopIdInput.value}`)
+        .then(res => res.json())
+        .then(data => {
+            stopNameDivElement.textContent = data.name;
+            for (const [bus, minutes] of Object.entries(data.buses)) {
+                let busInfoListItem = document.createElement('li');
+                busInfoListItem.textContent = `Bus ${bus} arrives in ${minutes} minutes`;
+                busesList.appendChild(busInfoListItem);
+            }
+        })
+        .catch(error => {
+            stopNameDivElement.textContent = 'Error';
+        });
+
+    stopIdInput.value = '';
+    // stopIdInput.focus();
+
+    function clearList() {
+        while (busesList.firstChild) {
+            busesList.firstChild.remove();
+        }
+    }
 }
