d80801c59ddaeddb91607e12849f5529a6516e47
ktopchiev <karlostopchiev@gmail.com>
2021-07-05 20:03:58 +0300

    bus schedule

--- JS-Applications/02.Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ JS-Applications/02.Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
+    let baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
+    let departButton = document.getElementById('depart');
+    let arriveButton = document.getElementById('arrive');
+    let currentBusStopId = 'depot';
+    let info = document.querySelector('div#info span.info');
 
     function depart() {
-        console.log('Depart TODO...');
+        fetchAndManageData(true, false);
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        fetchAndManageData(false, true);
+    }
+
+    function fetchAndManageData(departButtonState, arriveButtonState) {
+        fetch(`${baseUrl}/${currentBusStopId}`)
+            .then(res => res.json())
+            .then(data => {
+                info.textContent = switchInfoMessage(data);
+                departButton.disabled = departButtonState;
+                arriveButton.disabled = arriveButtonState;
+                currentBusStopId = data.next;
+            });
+    }
+
+    function switchInfoMessage(data) {
+        if (departButton.disabled) {
+            return `Arriving at ${data.name}`;
+        } else {
+            return `Next stop ${data.name}`;
+        }
     }
 
     return {
