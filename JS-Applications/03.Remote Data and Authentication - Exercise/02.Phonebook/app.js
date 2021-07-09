e8a6e01a078ffbc6ed6a48206d9df07d3556e419
ktopchiev <karlostopchiev@gmail.com>
2021-07-09 20:26:31 +0300

    phonebook solved

--- JS-Applications/03.Remote Data and Authentication - Exercise/02.Phonebook/app.js
+++ JS-Applications/03.Remote Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,62 +1,109 @@
-const url = 'http://localhost:3030/jsonstore/phonebook/';
-const [phonebook,notification ]= [document.getElementById('phonebook'),document.querySelector('.notification')];
-const hideMessage = ref => setTimeout(() => ref.textContent = '', 2000);
-
-function attachEvents() {
-    document.getElementById('btnLoad').addEventListener('click', loadData);
-    document.getElementById('btnCreate').addEventListener('click', createEntry);
-}
+let baseUrl = 'http://localhost:3030/jsonstore/phonebook';
+let loadButton = document.getElementById('btnLoad');
+let createContactButton = document.getElementById('btnCreate');
+let phonebookBody = document.getElementById('phonebook');
 
 attachEvents();
 
-async function loadData() {
-    phonebook.innerHTML = '';
-    const data = await request(url, 'get');
-    Object.values(data).map(createRow).forEach(x => phonebook.appendChild(x));
+function attachEvents() {
+    loadButton.addEventListener('click', loadContacts);
+    createContactButton.addEventListener('click', appendContact);
 }
 
-async function createEntry() {
-    const [person, phone] = [document.getElementById('person'), document.getElementById('phone')];
-    notification.textContent = '';
-    if (person.value == '' || phone.value == '') {
-        notification.textContent = 'All fields are required!';
-        hideMessage(notification);
+function loadContacts() {
+    clearPhonebookBody();
+    fetch(baseUrl)
+    .then(response => response.json())
+    .then(contacts => {
+        for (const key in contacts) {
+            let newContactElement = createContactElement(contacts[key]);
+            phonebookBody.appendChild(newContactElement);
+        }
+    })
+    .catch(error => console.error(error));
+}
+
+function appendContact() {
+    let personInput = document.getElementById('person');
+    let phoneInput = document.getElementById('phone');
+    let notification = document.querySelector('section.container p.notification');
+    let notificationMsg = validateInput(personInput, phoneInput);
+
+    if (notificationMsg) {
+        notification.textContent = notificationMsg;
         return;
+    } else {
+        notification.textContent = notificationMsg;
     }
-    const data = await request(url, 'post', { person: person.value, phone: phone.value });
-    phonebook.appendChild(createRow(data));
-    [person, phone].map(x => x.value = '');
+
+    const data = createNewDataObject(personInput.value, phoneInput.value);
+    fetch(baseUrl, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    .catch(error => console.error(error));
+    loadContacts();
+    clearInputFields(personInput, phoneInput);
 }
-async function deleteEntry(parent, id) {
-    if(confirm('Are you sure you want to delete this record?')) {
-        await request(url + id, 'delete');
-        parent.remove();
+
+function createNewDataObject(person, phone) {
+    return {
+        person: person,
+        phone: phone
     }
 }
 
-async function request(url, type, body) {
-    const option = {
-        method: type,
-        headers: { 'Content-Type': 'application/json' },
-    };
-    if (body) { option.body = JSON.stringify(body); }
-    const response = await fetch(url, option);
-    if (!response.ok) {
-        const { message } = response.json();
-        throw new Error(message);
+function createContactElement(data) {
+    let newTableRow = document.createElement('tr');
+    let nameDataElement = document.createElement('td');
+    let phoneDataElement = document.createElement('td');
+    let deleteContactData = document.createElement('td');
+    let deleteContactButton = document.createElement('button');
+
+    nameDataElement.textContent = data.person;
+    phoneDataElement.textContent = data.phone;
+    deleteContactButton.data_id = data._id;
+    deleteContactButton.textContent = 'Delete';
+    deleteContactButton.addEventListener('click', deleteContact);
+    deleteContactData.appendChild(deleteContactButton);
+
+    newTableRow.appendChild(nameDataElement);
+    newTableRow.appendChild(phoneDataElement);
+    newTableRow.appendChild(deleteContactData);
+
+    return newTableRow;
+}
+
+function deleteContact(e) {
+    let dataId = e.target.data_id;
+    e.target.parentNode.parentNode.remove();
+    fetch(`${baseUrl}/${dataId}`, {
+        method: 'DELETE',
+        headers: {'Content-type': 'application/json'}
+    })
+    .catch(error => console.error(error));
+}
+
+function clearPhonebookBody() {
+    while (phonebookBody.firstChild) {
+        phonebookBody.firstChild.remove();
     }
-    return response.json();
 }
 
-function create(type, ...content) {
-    const el = document.createElement(type);
-    content.map(c => typeof c === 'string' ? el.textContent = c : Array.isArray(c) ? el.setAttribute(c[0], c[1]) : el.appendChild(c));
-    return el;
-}
-function createRow({ person, phone, _id }) {
-    const button = create('button', ['data-id', _id], 'Delete');
-    const parent = create('tr', create('td', person), create('td', phone),create('td', button));
-    button.addEventListener('click', () => deleteEntry(parent, _id));
-    return parent;
+function clearInputFields(person, phone) {
+    person.value = '';
+    phone.value = '';
 }
 
+function validateInput(person, phone) {
+    if(person.value === '' && phone.value === '') {
+        return 'Fields cannot be empty!';
+    } else if(person.value === '' && phone.value !== '') {
+        return 'Person field cannot be empty!';
+    } else if(person.value !== '' && phone.value === '') {
+        return 'Phone field cannot be empty!'
+    } else {
+        return '';
+    }
+}
