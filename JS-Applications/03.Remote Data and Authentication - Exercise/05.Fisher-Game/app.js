46e1e3a9f80499881ff389be7ee5ed96db2686d1
ktopchiev <karlostopchiev@gmail.com>
2021-07-13 22:02:11 +0300

    .

--- JS-Applications/03.Remote Data and Authentication - Exercise/05.Fisher-Game/app.js
+++ JS-Applications/03.Remote Data and Authentication - Exercise/05.Fisher-Game/app.js
@@ -1,11 +1,18 @@
 let usersBaseUrl = 'http://localhost:3030/users';
 let catchesBaseUrl = 'http://localhost:3030/data/catches';
+
 let guestNavigation = document.getElementById('guest');
 guestNavigation.style.display = 'inline';
 let welcomeParagraph = document.querySelector('nav p.email span');
 welcomeParagraph.textContent = 'guest';
+
+let userNavigation = document.getElementById('user');
+
 let loginLink = document.querySelector('a#login');
+let registerLink = document.querySelector('a#register');
+
 let mainSection = document.getElementsByTagName('main')[0];
+
 let loginView = document.querySelector('#login-view');
 let views = document.querySelector('#views');
 mainSection.appendChild(views);
@@ -13,9 +20,13 @@ let registerView = document.querySelector('#register-view');
 let homeView = document.querySelector('#home-view');
 let mainFieldset = homeView.querySelector('fieldset#main');
 let loginForm = loginView.querySelector('form#login');
+
 let loadButton = homeView.querySelector('aside > button');
 let addForm = homeView.querySelector('#addForm');
 let catchesDiv = homeView.querySelector('#catches');
+let addNewCatchButton = addForm.querySelector('button');
+
+let registerForm = registerView.querySelector('form#register');
 
 attachEvents();
 
@@ -23,18 +34,28 @@ function attachEvents() {
     loginLink.addEventListener('click', showLoginView);
     loginForm.addEventListener('submit', logIn);
     loadButton.addEventListener('click', loadCatches);
+    registerLink.addEventListener('click', showRegisterView);
+    registerForm.addEventListener('submit', register);
+
 }
 
-function showLoginView(e) {
+function showLoginView() {
     views.style.display = 'inline-block';
     registerView.style.display = 'none';
     homeView.style.display = 'none';
 }
 
 function hideLoginView() {
-    views.style.display = 'flex';
+    views.style.display = 'inline-block';
     registerView.style.display = 'none';
     loginView.style.display = 'none';
+}
+
+function showRegisterView() {
+    hideLoginView();
+    views.style.display = 'inline-block';
+    registerView.style.display = 'inline-block';
+    homeView.style.display = 'none';
 }
 
 function logIn(e) {
@@ -60,6 +81,7 @@ function logIn(e) {
             }
         })
         .then(userData => {
+            changeNavBar(userData);
             localStorage.setItem('accessToken', userData.accessToken);
         })
         .catch(error => {
@@ -67,10 +89,47 @@ function logIn(e) {
         });
 }
 
+function register(e) {
+    e.preventDefault();
+    let formData = new FormData(e.currentTarget);
+    let data = {
+        email: formData.get('email'),
+        password: formData.get('password')
+    }
+
+    fetch(`${usersBaseUrl}/register`, {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.ok) {
+                notify(registerForm, response);
+                return response.json();
+            } else {
+                notify(registerForm, response);
+            }
+        })
+        .then(userData => {
+            changeNavBar(userData);
+            localStorage.setItem('accessToken', userData.accessToken);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function changeNavBar(data) {
+    welcomeParagraph.textContent = `${data.email}`;
+    guestNavigation.style.display = 'none';
+    userNavigation.style.display = 'inline';
+}
+
 function displayHomePage() {
     hideLoginView();
-    homeView.style.display = 'contents';
-    mainFieldset.style.display = 'block';
+    homeView.style.display = 'flex';
+    mainFieldset.style.display = 'flex';
+    addNewCatchButton.disabled = false;
 }
 
 function loadCatches() {
@@ -78,7 +137,7 @@ function loadCatches() {
         .then(response => response.json())
         .then(catches => {
             catches.forEach(catchData => {
-                let newCatchForm = createCatchForm(catchData);
+                let newCatchForm = cloneCatchForm(catchData);
                 console.log(newCatchForm);
                 catchesDiv.appendChild(newCatchForm);
             })
@@ -86,16 +145,16 @@ function loadCatches() {
         .catch(error => console.error(error));
 }
 
-function createCatchForm(data) {
+function cloneCatchFormTemplate(data) {
     let newCatchForm = addForm.cloneNode(true);
     newCatchForm.querySelector('legend').textContent = '';
 
-    document.querySelector('input[name="angler"]').value = data.angler;
-    document.querySelector('input[name="weight"]').value = data.weight;
-    document.querySelector('input[name="species"]').value = data.species;
-    document.querySelector('input[name="location"]').value = data.location;
-    document.querySelector('input[name="bait"]').value = data.bait;
-    document.querySelector('input[name="captureTime"]').value = data.captureTime;
+    newCatchForm.querySelector('input[name="angler"]').value = data.angler;
+    newCatchForm.querySelector('input[name="weight"]').value = data.weight;
+    newCatchForm.querySelector('input[name="species"]').value = data.species;
+    newCatchForm.querySelector('input[name="location"]').value = data.location;
+    newCatchForm.querySelector('input[name="bait"]').value = data.bait;
+    newCatchForm.querySelector('input[name="captureTime"]').value = data.captureTime;
 
     let updateCatchButton = document.createElement('button');
     updateCatchButton.classList.add('update');
@@ -103,7 +162,7 @@ function createCatchForm(data) {
     let deleteCatchButton = document.createElement('button');
     deleteCatchButton.classList.add('delete');
     deleteCatchButton.textContent = 'DELETE';
-    
+
     newCatchForm.querySelector('button').remove();
     newCatchForm.classList.add('catch');
     newCatchForm.id = "";
@@ -112,7 +171,7 @@ function createCatchForm(data) {
 
     newCatchForm.appendChild(updateCatchButton);
     newCatchForm.appendChild(deleteCatchButton);
-
+    //TODO: Only current user's buttons should be active.
     return newCatchForm;
 }
 
