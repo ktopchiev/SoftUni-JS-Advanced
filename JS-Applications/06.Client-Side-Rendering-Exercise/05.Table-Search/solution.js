2793ab0aad471d4251a619b0f5deb4a86f7e5e78
ktopchiev <karlostopchiev@gmail.com>
2021-07-26 14:24:08 +0300

    refactored

--- JS-Applications/06.Client-Side-Rendering-Exercise/05.Table-Search/solution.js
+++ JS-Applications/06.Client-Side-Rendering-Exercise/05.Table-Search/solution.js
@@ -4,29 +4,28 @@ import { tableTemplate } from './templates/tableBodyTemplate.js';
 
 let tableBody = document.querySelector('tbody');
 
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
-   let students;
+document.querySelector('#searchBtn').addEventListener('click', onClick);
+let students;
 
-   loadStudents();
+loadStudents();
 
-   async function loadStudents() {
-      students = Object.values(await getAllStudents());
-      render(tableTemplate(students), tableBody);
-   }
-
-   function onClick() {
-      let searchField = document.querySelector('#searchField');
-      let searchValue = searchField.value.toLowerCase();
-
-      let selectedStudents = students.map(s => Object.assign({}, s));
-      let matchedStudents = selectedStudents.filter(s => Object.values(s).some(val => val.toLowerCase().includes(searchValue)));
-      matchedStudents.forEach(student => {
-         student.class = 'select';
-      });
-      searchField.value = '';
-      render(tableTemplate(selectedStudents), tableBody);
-   }
+async function loadStudents() {
+   students = Object.values(await getAllStudents());
+   render(tableTemplate(students), tableBody);
 }
 
-solve();
+function onClick() {
+   let searchField = document.querySelector('#searchField');
+   let searchValue = searchField.value.toLowerCase();
+
+   let selectedStudents = students.map(s => Object.assign({}, s)); // Create new array with new objects from fetched data
+   let matchedStudents = selectedStudents
+      .filter(s => Object.values(s).some(val => val.toLowerCase().includes(searchValue))); // Match and filter values which are equal to search text
+
+   matchedStudents.forEach(student => {
+      student.class = 'select';
+   }); // For each matched student, add to it new property class with value select
+
+   searchField.value = '';
+   render(tableTemplate(selectedStudents), tableBody);
+}
